340a980444976bf2cf81e9f049678623646f7299
Aakifkhan7 <baloochaakifkhan@gmail.com>
2024-12-03 22:50:15 +0530

    some patterns done

---
+++ Floyd's_tringle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Floyd {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int rows = sc.nextInt();
+
+        for(int i = 0; i <= rows; i++){
+            
+            for(int j = 1; j <= i; j++){
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        
+    }
+}
---
+++ Reverse.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Reverse {
+    public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number of rows: ");
+        int rows = sc.nextInt();
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j = rows; j > i; j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

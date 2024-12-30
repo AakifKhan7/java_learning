67d2f0eaf4219a756408b600f431334f8a2c3fe7
Aakifkhan7 <baloochaakifkhan@gmail.com>
2024-12-30 17:01:08 +0530

    binery to decimal

---
+++ T3_learning/convert_binery.java
@@ -0,0 +1,29 @@
+package T3_learning;
+
+public class convert_binery {
+
+    int power(int b, int p) {
+        if (p == 0) {
+            return 1;
+        }
+        return b * power(b, p - 1);
+    }
+
+    int convert(int a, int count, int idx) {
+        if (a == 0) {
+            return 0;
+        }
+        int currElement = a % 10;
+        int currValue = currElement * power(2, idx);
+        return currValue + convert(a / 10, count - 1, idx + 1);
+    }
+
+    public static void main(String[] args) {
+        convert_binery ob1 = new convert_binery();
+        int a = 11011;
+
+        int count = String.valueOf(a).length();
+        int result = ob1.convert(a, count, 0);
+        System.out.println(result);
+    }
+}
